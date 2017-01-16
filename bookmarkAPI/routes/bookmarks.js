a4a8fe5264d3d8ff355aa107f313165f0974e4c0
Mario Mariani <marioslmariani@gmail.com>
2017-01-16 03:10:08 -0200

    Add Bookmark update

--- bookmarkAPI/routes/bookmarks.js
+++ bookmarkAPI/routes/bookmarks.js
@@ -41,4 +41,38 @@ bookmarks.post('/', function(req, res) {
   });
 });
 
+bookmarks.put('/:bookmarkId', function(req, res) {
+  var user = req.user;
+  var url = req.body.url;
+  var bookmarkId = req.params.bookmarkId;
+
+  if (!url || !bookmarkId) {
+    return res.status(400).json({
+      message: 'Url and BookmarkId required.'
+    });
+  }
+
+  Bookmark.findOne({
+    bookmarkId: bookmarkId,
+  })
+  .then(function(bookmark) {
+    if (!bookmark) {
+      return res.status(404).json({
+        message: 'Bookmark not found.'
+      });
+    }
+
+    bookmark.url = url;
+    bookmark.save()
+    .then(function(updatedBookmark) {
+      res.status(200).json({
+        message: 'Bookmark updated: ' + createdBookmark.url
+      });
+    });
+  })
+  .catch(function(err) {
+    return res.status(500).json(err);
+  });
+});
+
 module.exports = bookmarks;
